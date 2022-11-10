d51594c2a867dd4810c3a7a7bbd0dcc8679b4ef6
f3lancer <116956300+f3lancer@users.noreply.github.com>
2022-11-10 13:13:39 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let CloseMobail = document.querySelector('.menu-mobail__close');
+let MobailMenu = document.querySelector('.header');
+CloseMobail.onclick = function () {
+    // CloseMobail.classList.toggle('active')
+    MobailMenu.classList.toggle('active')
+
+}
